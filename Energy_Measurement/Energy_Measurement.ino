5b39439bb694733b95c0a34ae781205245831427
Cornell <castelic@tcd.ie>
2021-08-12 16:16:56 +0100

    Update Energy_Measurement.ino

--- Energy_Measurement/Energy_Measurement.ino
+++ Energy_Measurement/Energy_Measurement.ino
@@ -5,14 +5,14 @@
 File myFile;
 EnergyMonitor emon1;             // Create an instance
 double Rpower,Apower,V,I,Pf,samples;     //V and I are in RMS values.
-int stablize = 45;
+int stablize = 45;                      // takes # samples to stablize the arduino readings on start up
 int Time;
 bool initial;
 void setup()
 {
   Serial.begin(9600);
-  emon1.voltage(A3, 1080.6,1.15);  // Voltage: input pin, calibration, phase_shift
-  emon1.current(A2, 355.9);       // Current: input pin, calibration.
+  emon1.voltage(A3, 1065.98,1.15);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(A2, 367.2);       // Current: input pin, calibration.
   // new calibration = old * (correct reading/arduino reading)
   initial =  true;
   Rpower = 0;
@@ -32,7 +32,6 @@ void loop()
       emon1.calcVI(20,2000);
       Serial.print((double)i/stablize*100);
       Serial.println("% done..");
-      emon1.serialprint();
     }
     Serial.println("Output has stablized");
   initial = false;
@@ -46,10 +45,8 @@ void loop()
   I += emon1.Irms;
   Pf += emon1.powerFactor;
   ++samples;
-  
-  Store(emon1,myFile);
-  
-  if(millis() - Time >15000){
+  emon1.serialprint();
+  if(millis() - Time >15000){// log avg values every #s
     emon1.realPower = Rpower/samples;
     emon1.apparentPower = Apower/samples;
     emon1.Vrms = V/samples;
@@ -65,21 +62,23 @@ void loop()
     Pf = 0;
     samples = 0;
     Time = millis();
+    Serial.print("logged @:");
+    Serial.println(Time);
   }
 }
 void Store(EnergyMonitor emon1,File myFile){//function to store in SD card
   
-  Serial.print("Initializing SD card...");
+//  Serial.print("Initializing SD card...");
   if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
+//    Serial.println("initialization failed!");
     while (1);
   }
-  Serial.println("initialization done.");
+//  Serial.println("initialization done.");
   myFile = SD.open("test.txt", FILE_WRITE);
 
   if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.print(millis());
+//    Serial.print("Writing to test.txt...");
+    myFile.print(millis()/1000);
     myFile.print(",");
     myFile.print(emon1.realPower);
     myFile.print(",");
@@ -92,7 +91,7 @@ void Store(EnergyMonitor emon1,File myFile){//function to store in SD card
     myFile.println(emon1.powerFactor);
     
     myFile.close();
-    Serial.println("done.");
+//    Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
