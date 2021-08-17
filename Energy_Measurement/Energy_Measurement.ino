dd6e7c2ca8a105d0114d8eb95d3177a649b0a979
Cornell <castelic@tcd.ie>
2021-08-17 13:33:28 +0100

    big

--- Energy_Measurement/Energy_Measurement.ino
+++ Energy_Measurement/Energy_Measurement.ino
@@ -5,14 +5,18 @@
 File myFile;
 EnergyMonitor emon1;             // Create an instance
 double Rpower,Apower,V,I,Pf,samples;     //V and I are in RMS values.
-int stablize = 30;                      // takes # samples to stablize the arduino readings on start up
+int stablize =45;                // takes # samples to stablize the arduino readings on start up
+double Vcal = 692.52;//692.52
+double Ical = 144.55;//144.55
+double Pfcal = 1.15;
 int Time;
 bool initial;
 void setup()
 {
   Serial.begin(9600);
-  emon1.voltage(A3, 692.52,1.15);  // Voltage: input pin, calibration, phase_shift
-  emon1.current(A2, 144.55);       // Current: input pin, calibration.
+  pinMode(2, OUTPUT);
+  emon1.voltage(A6, Vcal,Pfcal);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(A1, Ical);       // Current: input pin, calibration.
   // new calibration = old * (correct reading/arduino reading)
   initial =  true;
   Rpower = 0;
@@ -21,15 +25,16 @@ void setup()
   I = 0;
   Pf = 0;
   samples = 0;
-  Time = millis();
-}
+  Time = millis();}
 
 void loop()
 { 
   if (initial){
     Serial.println("Starting to stablize...");
     for( int i = 0; i < stablize;i++){
+      digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level) 
       emon1.calcVI(20,2000);
+      digitalWrite(2, HIGH);
       Serial.print((double)i/stablize*100);
       Serial.println("% done..");
     }
@@ -38,7 +43,12 @@ void loop()
   }
   //__________________________________________
   //avg values
+  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level) 
   emon1.calcVI(20,2000);
+  digitalWrite(2, HIGH);    // turn the LED off by making the voltage LOW
+
+
+  
   Rpower += emon1.realPower;
   Apower += emon1.apparentPower;
   V += emon1.Vrms;
@@ -47,32 +57,33 @@ void loop()
   ++samples;
   
   emon1.serialprint();
-  
-  if(millis() - Time >15000){// log avg values every #s
-    emon1.realPower = Rpower/samples;
-    emon1.apparentPower = Apower/samples;
-    emon1.Vrms = V/samples;
-    emon1.Irms = I/samples;
-    emon1.powerFactor = Pf/samples;
-    
-    Store(emon1,myFile);
-    
-    Rpower = 0;
-    Apower = 0;
-    V = 0;
-    I = 0;
-    Pf = 0;
-    samples = 0;
-    Time = millis();
-    Serial.print("logged @:");
-    Serial.println(Time);
-  }
+  Store(emon1,myFile);
+//  if(millis() - Time >15000){// log avg values every #s
+//    emon1.realPower = Rpower/samples;
+//    emon1.apparentPower = Apower/samples;
+//    emon1.Vrms = V/samples;
+//    emon1.Irms = I/samples;
+//    emon1.powerFactor = Pf/samples;
+//    
+//    Store(emon1,myFile);
+//    
+//    Rpower = 0;
+//    Apower = 0;
+//    V = 0;
+//    I = 0;
+//    Pf = 0;
+//    samples = 0;
+//    Time = millis();
+//    Serial.print("logged @:");
+//    Serial.println((double)Time/1000);
+//  }
 }
 void Store(EnergyMonitor emon1,File myFile){//function to store in SD card
   
 //  Serial.print("Initializing SD card...");
   if (!SD.begin(10)) {
     Serial.println("initialization failed!");
+    digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level) 
     while (1);
   }
 //  Serial.println("initialization done.");
